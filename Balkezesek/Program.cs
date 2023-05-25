2bc0c48567dccdf99ea5be355cdfe10d4c19f727
Stigi Bálint <Stigi Blint@REVISION-PC>
2023-05-25 12:32:25 +0200

    done

--- Balkezesek/Program.cs
+++ Balkezesek/Program.cs
@@ -7,34 +7,40 @@ using Balkezesek;
 List<BalkezesekClass> adatok = new List<BalkezesekClass>();
 try
 {
-    string[] sorok = File.ReadAllLines("balkezesek.csv");
-    for (int i = 1; i < sorok.Length; i++)
-    {
-        adatok.Add(new BalkezesekClass(sorok[i]));
-    }
+    File.ReadAllLines("balkezesek.csv").Skip(1).ToList().ForEach(x => adatok.Add(new BalkezesekClass(x)));
 }
 catch (IOException e)
 {
     Console.WriteLine($"Hiba történt a fájl olvasása közben: {e.Message}");
 }
-Console.WriteLine("3. feladat: A forrásállományban {0} adatsor található.", adatok.Count);
-var jatekosok1999Oktober = adatok.Where(j => j.UtolsoDatum.EndsWith("1999-10"));
-Console.WriteLine("4. feladat: :");
+Console.WriteLine($"3. feladat: {adatok.Count}");
+
+var jatekosok1999Oktober = adatok.Where(j => j.UtolsoDatum.Contains("1999-10"));
+
+Console.WriteLine("4. feladat:");
 foreach (var jatekos in jatekosok1999Oktober)
 {
     double magassagCm = jatekos.JatekosMagassag * 2.54;
-    Console.WriteLine("Név: {0}, Magasság: {1:F1} cm", jatekos.Nev1, magassagCm);
+    Console.WriteLine($"{jatekos.Nev1} {magassagCm}".PadLeft(20));
 }
-Console.WriteLine("5. feladat: Kérem adjon meg egy évszámot (1990 és 1999 között):");
+
+Console.WriteLine("5. feladat: ");
 int evszam = int.Parse(Console.ReadLine());
-
-while (true)
+do
 {
-        if (evszam >= 1990 && evszam <= 1999)
-            break;
-   
-    Console.WriteLine("Hibás évszám! Kérem adjon meg egy érvényes évszámot (1990 és 1999 között):");
-}
+    if (!(evszam >= 1990 && evszam <= 1999))
+    {
+        Console.WriteLine($"Hibás adat!Kérek egy 1990 és 1999 közötti évszámot!: {evszam}");
+        evszam = int.Parse(Console.ReadLine());
+    }
+    else
+    {
+        int jatekosokSzama = adatok.Count(j => j.ElsoDatum.StartsWith(evszam.ToString()) || j.UtolsoDatum.StartsWith(evszam.ToString()));
+        Console.WriteLine("Az {0}. évben {1} játékos játszott.", evszam, jatekosokSzama);
+        break;
+    }
+} while (true);
 
-int jatekosokSzama = adatok.Count(j => j.ElsoDatum.StartsWith(evszam.ToString()) || j.UtolsoDatum.StartsWith(evszam.ToString()));
-Console.WriteLine("Az {0}. évben {1} játékos játszott.", evszam, jatekosokSzama);
+
+var atlagosSuly = adatok.Where(j => j.ElsoDatum.StartsWith(evszam.ToString()) || j.UtolsoDatum.StartsWith(evszam.ToString())).Select(j => j.JatekosSulya).Average();
+Console.WriteLine($"6. feladat:{Math.Round(atlagosSuly,2)} ");
